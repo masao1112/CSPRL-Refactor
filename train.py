810ae4fd38be15e54805334ca85aa15fcade4362
masao1112 <thaianh494@gmail.com>
2026-03-25 13:09:09 +0700

    chore: update hidden dims

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
             x, y = ts2xy(load_results(self.log_dir), 'timesteps')
             if len(x) > 0:
                 # Mean training reward over the last 100 episodes
-                my_mean_reward = np.mean(y[-3:])
+                my_mean_reward = np.mean(y[-10:])
                 if self.verbose > 0:
                     print("Num timesteps: {}".format(self.num_timesteps))
                     print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(
@@ -91,9 +91,9 @@ if __name__ == '__main__':
     """
     os.makedirs(log_dir, exist_ok=True)
     env = Monitor(env, os.path.join(log_dir, "monitor.csv"))
-    policy_kwargs = dict(net_arch=[128, 128]) # hidden layers
+    policy_kwargs = dict(net_arch=[256, 256]) # hidden layers
     model = DQN("MlpPolicy", env, verbose=1, batch_size=128, buffer_size=10000, learning_rate=1e-5,
-                exploration_initial_eps=1, exploration_final_eps=0.00, exploration_fraction=0.2, policy_kwargs=policy_kwargs,
+                exploration_initial_eps=0.95, exploration_final_eps=0.05, exploration_fraction=0.2, policy_kwargs=policy_kwargs,
                 device='cuda' if torch.cuda.is_available() else 'cpu', seed=seed)
     callback = SaveOnBestTrainingRewardCallback(check_freq=400, my_log_dir=log_dir, my_modelname=modelname)
     model.learn(total_timesteps=200000, log_interval=10 ** 4, callback=callback)
